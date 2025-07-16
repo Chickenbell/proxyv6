fa8ae04d31572f01d494e0fa6108c5a38bff21ac
Chickenbell <117949164+Chickenbell@users.noreply.github.com>
2025-07-16 08:58:31 +0700

    Create cleanup_and_regenerate.sh

---
+++ cleanup_and_regenerate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# cleanup_and_regenerate.sh
+systemctl stop 3proxy
+pkill -f 3proxy
+iptables -F
+ip -6 addr flush dev $(ip route get 8.8.8.8 | awk '{print $5}')
+rm -f /usr/local/etc/3proxy/3proxy.cfg /home/chickenbell/data.txt /home/chickenbell/proxy.txt
+systemctl restart network
+sleep 5
+./v6_vultr_v2.sh
